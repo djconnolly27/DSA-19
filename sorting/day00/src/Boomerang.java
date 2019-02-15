9106711c2dfbcd32a8cfd9dda698d986853462e1
Daniel Connolly <djconnolly27@gmail.com>
2019-02-14 22:40:17 -0500

    Adds completed sorting day 0

--- sorting/day00/src/Boomerang.java
+++ sorting/day00/src/Boomerang.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
+import java.lang.Math;
 public class Boomerang {
 
     public static int numberOfBoomerangs(int[][] points) {
-        // TODO
-        return 0;
+        HashMap<Double, Integer> boomHash = new HashMap<>();
+        int total = 0;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i==j) { continue; }
+                double dist = Math.sqrt(Math.pow((points[i][0] - points[j][0]),2) + Math.pow((points[i][1] - points[j][1]),2));
+                boomHash.put(dist, boomHash.getOrDefault(dist, 0)+1);
+            }
+            for (int val: boomHash.values()) {
+                total += val*(val-1);
+            }
+            boomHash.clear();
+        }
+        return total;
     }
 }
 
--- sorting/day00/src/MergeSort.java
+++ sorting/day00/src/MergeSort.java
@@ -8,17 +8,26 @@ public class MergeSort extends SortAlgorithm {
      * a left and a right portion, sort them, and then merge them together.
      * Use Insertion Sort if the length of the array is <= INSERTION_THRESHOLD
      *
-     * TODO
-     * Best-case runtime:
-     * Worst-case runtime:
-     * Average-case runtime:
      *
-     * Space-complexity:
+     * Best-case runtime: O(nlog(n))
+     * Worst-case runtime: O(nlog(n))
+     * Average-case runtime: O(nlog(n))
+     *
+     * Space-complexity: O(n)
      */
     @Override
     public int[] sort(int[] array) {
-        // TODO
-        return new int[0];
+        if (array.length<=1) {
+            return array;
+        }
+        int[] left = new int[array.length/2];
+        int[] right = new int[array.length-left.length];
+        System.arraycopy(array, 0, left, 0, array.length/2);
+        System.arraycopy(array, array.length/2, right,0, array.length-left.length);
+        int[] sortL = sort(left);
+        int[] sortR = sort(right);
+        array = merge(sortL, sortR);
+        return array;
     }
 
     /**
@@ -26,8 +35,25 @@ public class MergeSort extends SortAlgorithm {
      * all elements in a and b. A test for this method is provided in `SortTest.java`
      */
     public int[] merge(int[] a, int[] b) {
-        // TODO
-        return new int[0];
+        int indexa = 0;
+        int indexb = 0;
+        int[] arr = new int[a.length+b.length];
+        for (int i = 0; i < a.length+b.length; i++) {
+            if (indexa >= a.length) {
+                arr[i] = b[indexb];
+                indexb++;
+            } else if (indexb >= b.length) {
+                arr[i] = a[indexa];
+                indexa++;
+            } else if (a[indexa] < b[indexb]) {
+                arr[i] = a[indexa];
+                indexa++;
+            } else {
+                arr[i] = b[indexb];
+                indexb++;
+            }
+        }
+        return arr;
     }
 
 }
